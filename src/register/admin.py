5284a26434a5fb80718dffdc27c708af275a27df
Sumit-320 <202201320@daiict.ac.in>
2025-01-19 16:12:09 +0530

    admin.py config

--- src/register/admin.py
+++ src/register/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+class CustomAdmin(UserAdmin):
+      # list_display includes the fields that are present in the User model
+    list_display = ('username', 'email', 'f_name', 'l_name', 'type','is_admin')
+    ordering = ('-date_of_register',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User,CustomAdmin)
