381bf00f7fe27e68688de7dbf7eda039855c443c
Sumit-320 <202201320@daiict.ac.in>
2025-01-25 14:42:03 +0530

    user profile db

--- src/register/admin.py
+++ src/register/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Profile2
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class CustomAdmin(UserAdmin):
@@ -11,3 +11,4 @@ class CustomAdmin(UserAdmin):
     fieldsets = ()
 
 admin.site.register(User,CustomAdmin)
+admin.site.register(Profile2)
--- src/register/models.py
+++ src/register/models.py
@@ -71,4 +71,24 @@ class User(AbstractBaseUser): # custom user --> not the django default
         return self.is_admin
     def has_module_perms(self,app_label):
         return True
+
+# upon creation of user, profile will not be created automatically!!
+
+class Profile2(models.Model):  # for customer
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null=True)
+    profile_pic= models.ImageField(upload_to='users/profile_pics',blank=True,null=True) 
+    bg_pic= models.ImageField(upload_to='users/bg_pics',blank=True,null=True) 
+    address_line1 = models.CharField(max_length=50,blank=True,null =True)
+    address_line2 = models.CharField(max_length=50,blank=True,null =True)
+    country = models.CharField(max_length=20,blank=True,null =True)
+    state = models.CharField(max_length=20,blank=True,null =True)
+    city = models.CharField(max_length=20,blank=True,null =True)
+    pin = models.CharField(max_length=6,blank=True,null =True)
+    latitude = models.CharField(max_length=20,blank=True,null =True)
+    longitude = models.CharField(max_length=20,blank=True,null =True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
     
